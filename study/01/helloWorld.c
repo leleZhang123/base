563fcbdcdbab2f071ff3d40bc90dd0b598452e84
Lelel.Zhang <zlwajq123@163.com>
2021-04-08 11:17:52 +0800

    tan--1

---
+++ study/01/helloWorld.c
@@ -0,0 +1,6 @@
+#include<stdio.h> //头文件standard input and output
+void main()       //主函数
+{
+    printf("Hello World!!!"); //输出hello world
+    return;                     //返回为空
+}
---
+++ study/01/sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;                //声明a,b,sum
+    a=123;                      //定义a赋值为123
+    b=456;                      //定义b赋值为456
+    sum=a+b;                    //sum取和
+    printf("sum is %d\n",sum);  //输出格式化的sum   %d 为整数型格式
+    return 0;                   //返回0值
+}   
